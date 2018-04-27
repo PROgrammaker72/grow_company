429647e195ebbe920a44250a429d23bb3127baec
PROgrammaker72 <o.o.diordiy@gmail.com>
2018-04-27 18:20:21 +0300

    adition better

--- Addition/js/preload.js
+++ Addition/js/preload.js
@@ -3,3 +3,19 @@
 function ownpreloader(){
 	document.getElementById("preloader").style.display = "none";
 }
+
+let f = document.getElementsByClassName("house");
+
+function houseTransform() {
+  console.log(f[0].getElementsByClassName("active"));
+  if ((f[0].getElementsByClassName("active")[0].getAttribute("class").toString().indexOf("dot1") !== -1) || (f[0].getElementsByClassName("active")[0].getAttribute("class").toString().indexOf("dot2") !== -1)) {
+    document.getElementById("house_animation").style.flexDirection = "row-reverse";
+  }
+  else {
+    document.getElementById("house_animation").style.flexDirection = "row";
+  }
+}
+
+document.getElementById("house_animation").addEventListener("click", function() {
+  houseTransform();
+})
